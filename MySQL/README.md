6ef13db0473a04e27fdcb549720b9f43301bb5ff
callor <callor@callor.com>
2021-05-17 11:50:53 +0900

    MySQL

---
+++ MySQL/README.md
@@ -0,0 +1,21 @@
+# MySQL root Password
+!Biz12341234
+
+# MySQL의 User
+* Oracle의 user는 Schema를 포함하는 개념인데 MySQL 그냥 login하는 사용자 이다
+* Oracle의 user는 기본적으로 아무런 권한이 없는 상태이지만, MySQL은 사용자를 등록하는 순간 많은 권한을 갖게 된다
+* Oracle은 User 자체가 하나의 DataBase인데 MySQL은 사용자와 DataBase는 직접적으로 연관이 없다
+* MySQL Comm Version에서는 별도로 사용자를 등록하지 않고 일반적으로 root 권한으로 사용한다.
+
+## root 권한
+* Unix(Linux)운영체제에서 최고 권한을 갖는 사용자이다. 
+* MySQL은 태생이 Linux 홤경이어서 Linux와 유사한 개념을 많이 사용한다.
+
+## DataBase
+* Oracle의 TableSpace와 유사한 개념으로 일반적으로 표준 SQL을 사용하는 데이터베이스 소프트웨어의 저장소 개념
+* CREATE 명령을 사용하여 Data 저장소 생성
+
+
+
+
+
---
+++ MySQL/Root(2021-05-17-Test).sql
@@ -0,0 +1,134 @@
+-- 현재 존재하는  DataBase 를 보여달라
+SHOW databases;
+-- 지금부터 MYSQL DataBase 를 사용하겠다
+-- 사용자와 연관 없이 기본적으로 사용할
+-- DataBase 를 지정하여 사용할 준비
+USE MYSQL;
+-- 현재 접속한 Database(mysql)에 있는
+-- 모든  table 을 보여달라
+SHOW tables;
+
+-- myDB라는 Database( 데이터 저장소 ) 생성
+CREATE DATABASE myDB;
+-- 생성된 저장소 확인
+SHOW DATABASES;
+
+-- MySQL 에서는 사용할 DB를 open 하기
+-- USE 명령을 사용하여 DB open
+USE MyDB;
+-- 현재 DB(MyDB)에 있는 모든 Table 을 
+-- 보여달라
+SHOW TABLES;
+
+-- MySQL에서는 일련번호와 관련된 칼럼에
+-- AUTO_INCREMENT 옵션을 설정하면
+-- INSERT 할때 값을 지정하지 않아도
+-- 자동으로 ID, SEQ 값을 생성하여
+-- 칼럼에 추가하여 준다.
+CREATE TABLE tbl_test (
+	id BIGINT PRIMARY KEY 
+			AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    tel VARCHAR(20),
+    addr VARCHAR(125)
+);
+SHOW TABLES ;
+DESC tbl_test;
+
+INSERT INTO tbl_test(name, tel, addr)
+VALUES('홍길동','010-111-1111','서울시');
+SELECT * FROM tbl_test;
+-- MySQL은  Linux 철학을 유지 하고 있기 때문에
+-- 명령이 정상으로 수행되면
+-- 아무런 메시지도 보이지 않는다
+CREATE TABLE tbl_books (
+	bk_isbn	CHAR(13)		PRIMARY KEY,
+	bk_comp	VARCHAR(15)	NOT NULL,	
+	bk_title	VARCHAR(125)	NOT NULL,	
+	bk_author	VARCHAR(50)	NOT NULL,	
+	bk_trnans	VARCHAR(20)	,	
+	bk_date	VARCHAR(10)	,	
+	bk_pages	INT	,	
+	bk_price	INT		
+);
+SHOW TABLES ;
+DESC tbl_books;
+
+DROP TABLE tbl_books;
+SELECT * FROM tbl_books;
+
+SELECT COUNT(*) 
+FROM tbl_books;
+
+--  도서 가격이 20000원 이상인 데이터
+SELECT * 
+FROM tbl_books
+WHERE bk_price >= 20000;
+
+-- 도서 가격이 만원 이상 2만원 이하
+SELECT *
+FROM tbl_books
+WHERE bk_price 
+	BETWEEN 10000 AND 20000;
+
+SELECT *
+FROM tbl_books
+WHERE bk_price >= 10000 
+		AND bk_price <= 20000;
+        
+--  도서명에 '왕' 문자열이 있는 데이터
+SELECT *
+FROM tbl_books
+WHERE bk_title LIKE '%왕%';		
+
+-- Java 등 코딩에서 중간 문자열 검색
+-- oracle : '%' || '왕' || '%'
+-- MySQL : CONCAT('%', '왕' , '%')
+SELECT *
+FROM tbl_books
+WHERE bk_title 
+LIKE CONCAT('%', '왕', '%'); 
+
+-- 날짜 칼럼의 앞에 4글자만 보여라
+SELECT LEFT(bk_date,4)
+FROM tbl_books;
+
+-- 발행일이 2018년인 도서들
+SELECT *
+FROM tbl_books
+WHERE LEFT(bk_date,4) = '2018';
+-- 전체 데이터를 날짜 순으로 보여라
+SELECT *
+FROM tbl_books
+ORDER BY bk_date;
+
+-- 도서명을 역순으로 정렬하여 보여라
+SELECT *
+FROM tbl_books
+ORDER BY bk_title DESC;
+
+-- 처음 3개의 데이터만 보여라
+SELECT *
+FROM tbl_books
+LIMIT 3;
+
+-- 4(0 부터 시작하여 3번)번째 
+-- 데이터 부터 2개
+-- 게시판 등 코딩에서 pagination 을 구현할때
+-- 사용하는 방법
+SELECT *
+FROM tbl_books
+LIMIT 3, 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
