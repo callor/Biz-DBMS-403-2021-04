b6609a55eb54ee0b05d3123cd243be385c7d1c48
callor <callor@callor.com>
2021-06-01 11:43:43 +0900

    Finale

--- Oracle/TestUser(2021-06-01).sql
+++ Oracle/TestUser(2021-06-01).sql
@@ -150,10 +150,70 @@ SELECT * FROM
 )
 WHERE SUBSTR(출판일,0,4) = '2018';
 
+-- tbl_books와 tbl_company, tbl_books와 tbl_author FK 설정
+-- bk_comp와 cp_code         bk_author와 au_code
+ALTER TABLE tbl_books -- 누구한테
+ADD CONSTRAINT FK_COMP  -- 이름
+FOREIGN KEY (bk_comp) -- 누구의 칼럼
+REFERENCES tbl_company(cp_code); -- 참조대상
 
+ALTER TABLE tbl_books -- 누구한테
+ADD CONSTRAINT FK_AUTO  -- 이름
+FOREIGN KEY (bk_author) -- 누구의 칼럼
+REFERENCES tbl_author(au_code); -- 참조대상
 
+-- PK
+-- 개체물결성을 보장하기 위한 조건
+-- 내가 어떤 데이터를 수정, 삭제할 때
+-- 수정하거나 삭제해서는 안되는 데이터는 유지하면서
+-- 반드시 수정하거나 삭제하는 데이터는 수정, 삭제가 된다
+-- 수정이상, 삭제이상을 방지하는 방법
+-- 중복된데이터는 절대 추가될수 없다 : 삽입이상을 방지하는 방법
 
+-- FK
+-- 두 개이상의 table을 연결하여 view(조회)를 할때
+-- 어떤 데이터가 NULL 값으로 보이는 것을 방지하기 위한 조치
 
+-- Child(tbl_books):bk_comp         Parent(tbl_comp):cp_code
+-- 있을수 있고, 추가 가능      <<   있는 코드
+-- 있어서는 안되고 추가 불가능 <<   없는 코드
+-- 있는 코드                   >>   코드 삭제 불가능
+-- 있는 코드                   >>   반드시 있어야 한다.
 
+-- 리쳐드 쇼튼의 연락처가 010-6676-6428에서
+--  010-9898-6428로 변경이 되었다
+-- 리쳐드 쇼튼의 연락처를 변경해 보기
+UPDATE tbl_author
+SET au_tel = '010-9898-6428'
+WHERE au_name = '리쳐드 쇼튼';
+
+UPDATE tbl_author
+SET au_tel = '010-9898-6428'
+WHERE au_tel = '010-6676-6428';
+
+-- 정보를 수정, 삭제하는 절차
+-- 내가 수정, 삭제하고자 하는 데이터가 어떤 상태인지 조회
+SELECT * 
+FROM tbl_author
+WHERE au_name = '리쳐드 쇼튼';
+
+SELECT * 
+FROM tbl_author
+WHERE au_name LIKE '리처드%';
+
+SELECT *
+FROM tbl_author;
+
+-- 수정하고자 하는  리처드 쇼튼의 PK 를 확인했다
+-- 수정, 삭제하고자 할때는 먼저 대상 데이터의 PK를 확인하고
+-- PK를 WHERE 절에 포함하여 UPDATE, DELETE를 수행하자
+UPDATE tbl_author
+SET au_tel = '010-9898-6428'
+WHERE au_code = 'A0006';
+
+-- 실무에서 UPDATE, DELETE를 2개이상 레코드에
+-- 동시에 적용하는 것은 매우 위험한 코드이다
+-- 꼭 필요한 경우가 아니면
+-- UPDATE, DELETE는 PK를 기준으로 한개씩 수행하는 것이 좋다
 
 
